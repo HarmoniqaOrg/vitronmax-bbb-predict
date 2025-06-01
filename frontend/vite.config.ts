df35f7b9f46ef2c52625f400e21b02e7c8c82592
Pierre Neuman <pierre.neuman@gmail.com>
2025-06-01 23:57:41 +0200

    same

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import wasm from "vite-plugin-wasm";
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 import { componentTagger } from "lovable-tagger";
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
@@ -16,6 +17,14 @@ export default defineConfig(({ mode }) => ({
     react(),
     nodePolyfills(),
     wasm(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'node_modules/@rdkit/rdkit/dist/RDKit_minimal.wasm',
+          dest: 'assets'
+        }
+      ]
+    }),
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
